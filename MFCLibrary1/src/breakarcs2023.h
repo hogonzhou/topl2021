582cae9f2dc33d7b8202f44cf9c5318d56c01fd8
hua chou <ganghua2018@hotmail.com>
2022-12-31 11:34:08 +0800

    a1

--- MFCLibrary1/src/breakarcs2023.h
+++ MFCLibrary1/src/breakarcs2023.h
@@ -17,6 +17,11 @@ public:
 	}
 
 private:
+	bool isRadiusValid()
+	{
+		//检查半径值r是否合法：不能小于0.00001
+		return m_radiu < tol;
+	}
 	double calChord()
 	{
 		//已知拱高，半径,求弦长一半;
@@ -45,5 +50,5 @@ private:
 	double m_radiu = 0.0;
 	double m_bias = 0.0;
 	double m_chord = 0.0;
-	const double tol = 0.0001; //0.1um作为默认误差标准,用来对本类浮点数做比较;
+	const double tol = 0.00001; //0.1um作为默认误差标准,用来对本类浮点数做比较;
 };
